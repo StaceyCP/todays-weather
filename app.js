220034f42f9fa9ea38a75c4e73715e200cfc5722
Stacey <stacey6105@gmail.com>
2022-11-02 01:32:06 +0000

    Install andf configure EJS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
@@ -6,31 +7,35 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
     const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=YOUR-API-KEY&units=metric"
-    https.get(weatherApi, function(response){
-        response.on("data", function(data){
+    https.get(weatherApi, (response) => {
+        response.on("data", (data) => {
             const weatherData = JSON.parse(data);
         })
     })
-    res.sendFile(__dirname + "/index.html");
+    res.render("home");
 })
 
-app.post("/", function(req, res){
+app.get("/weather", (req, res) => {
+    res.render("weather")
+});
+
+app.post("/", (req, res) => {
     const query = req.body.cityName
     const apiKey = "YOUR-API-KEY"
     const unit = "metric"
     const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
-    https.get(weatherAPI, function(response){
-        response.on("data", function(data){
+    https.get(weatherAPI, (response) => {
+        response.on("data", (data) => {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDesc = weatherData.weather[0].description
             const weatherIcon = weatherData.weather[0].icon
             const imgURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
-            res.write("<h1>Temp " + temp + "</h1>");
-            res.write("<h2>" + weatherDesc + "</h2>");
-            res.write("<img src=" + imgURL + ">");
+            res.redirect("/weather");
         })
     })
 })
